786cfaacf0ba3df090315923cd702f0b563b208b
Alexis Launay <yorunohikage@gmail.com>
2016-11-12 17:53:31 +0100

    Make scrolling world

---
+++ Assets/GroundHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundHandler : MonoBehaviour
+{
+	public float speed = 1.0f;
+
+	void Update ()
+	{
+		for (int i = 0; i < transform.childCount; i++) {
+			transform.GetChild(i).Translate (Vector3.left * speed * Time.deltaTime);
+		}
+
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+		GameObject leftPlate = transform.GetChild(0).gameObject;
+		if (!GeometryUtility.TestPlanesAABB(planes, leftPlate.transform.GetChild(0).GetComponent<Collider>().bounds)) {
+			leftPlate.transform.SetAsLastSibling ();
+
+			leftPlate.transform.position = new Vector3(
+				transform.GetChild(1).position.x + transform.GetChild(1).GetChild(0).GetComponent<Collider>().bounds.size.x,
+				0,
+				0
+			);
+		}
+	}
+}
