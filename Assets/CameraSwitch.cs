61f6832fe280a98bb18be401d3153e50dfc12a38
Alexis Launay <yorunohikage@gmail.com>
2016-11-12 01:48:12 +0100

    Initial project with basic inputs

---
+++ Assets/CameraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour
+{
+	public float speed;
+	public bool isCameraTop = true;
+
+	void Update ()
+	{
+		Vector3 cameraTopPos = new Vector3 (0, 12, 0);
+		Vector3 cameraSidePos = new Vector3 (0, -6, -15);
+
+		if (Input.GetKeyUp ("space")) {
+			isCameraTop = !isCameraTop;
+		}
+
+		Vector3 targetDir = -(isCameraTop ? cameraTopPos : cameraSidePos) - transform.position;
+
+		Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, speed * Time.deltaTime, 0.0f);
+		transform.rotation = Quaternion.LookRotation (newDir);
+	}
+}
---
+++ Assets/SpaceshipMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax, zMin, zMax;
+};
+
+public class SpaceshipMovement : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject center;
+
+	void FixedUpdate ()
+	{
+		bool isCameraTop = center.GetComponent<CameraSwitch> ().isCameraTop;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (
+			                   moveHorizontal,
+			                   isCameraTop ? 0.0f : moveVertical,
+			                   isCameraTop ? moveVertical : 0.0f
+		                   );
+		GetComponent<Rigidbody> ().velocity = movement * speed;
+
+		float xPosition = Mathf.Clamp (GetComponent<Rigidbody> ().position.x, boundary.xMin, boundary.xMax);
+		if (isCameraTop) {
+			float zPosition = Mathf.Clamp (GetComponent<Rigidbody> ().position.z, boundary.zMin, boundary.zMax);
+			GetComponent<Rigidbody> ().position = new Vector3 (xPosition, 5.0f, zPosition);
+			GetComponent<Rigidbody> ().rotation = Quaternion.Euler (GetComponent<Rigidbody> ().velocity.z * -tilt, 180.0f, 90.0f);
+		} else {
+			float yPosition = Mathf.Clamp (GetComponent<Rigidbody> ().position.y, boundary.yMin, boundary.yMax);
+			GetComponent<Rigidbody> ().position = new Vector3 (xPosition, yPosition, 0.0f);
+			GetComponent<Rigidbody> ().rotation = Quaternion.Euler (0.0f, 180.0f, 90.0f + GetComponent<Rigidbody> ().velocity.y * -tilt);
+		}
+	}
+}
